f9d427fcdd61f73f0488c37e89f1d323a922b628
tutuliao <102414599+tutuliao@users.noreply.github.com>
2024-07-09 15:32:13 +0800

    删掉一些多余的依赖

--- ActivitySceneTransitionBasic/app/src/main/java/com/example/android/activityscenetransitionbasic/DetailActivity.java
+++ ActivitySceneTransitionBasic/app/src/main/java/com/example/android/activityscenetransitionbasic/DetailActivity.java
@@ -21,19 +21,15 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
 import android.os.Build;
 import android.os.Bundle;
-import android.transition.Explode;
 import android.transition.Transition;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.ViewCompat;
-
-import com.bumptech.glide.GenericTransitionOptions;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.squareup.picasso.Picasso;
